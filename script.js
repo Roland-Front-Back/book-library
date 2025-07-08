65c1febb52b2a2e48a86a77b4e3a1d990d72f195
Roland Amoguis <185738272+Roland-Front-Back@users.noreply.github.com>
2025-07-08 21:14:08 +0800

    Add styles and layout
    Adding style to the html skeletal structure in css file
    buttons, icons, bgColors, fonts, width, height, and positions

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-function Book(title, author, pages, read, unread) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.unread = unread;
+const myLibrary = [];
 
-  this.info = function () {
-    return (
-      "Book name: " +
-      this.title +
-      " " +
-      "Author: " +
-      this.author +
-      " " +
-      "Pages: " +
-      this.pages
-    );
-  };
+function Book(title, author, pages, read, unread, id) {
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read),
+    (this.unread = unread),
+    (this.id = id),
+    (this.info = function () {
+      return (
+        "Book name: " +
+        this.title +
+        " " +
+        "Author: " +
+        this.author +
+        " " +
+        "Pages: " +
+        this.pages
+      );
+    });
 }
 
 const theHobbit = new Book("The hobbit", "Rowan", 256);
 
-console.log(theHobbit.info());
+console.log(theHobbit);
+
+function addBookToLibrary(title, author, pages, read, unread, id) {
+  let randomId = self.crypto.randomUUID();
+}
