7f960fc76f3ee63cb29d048955119ad088d877be
Roland Amoguis <185738272+Roland-Front-Back@users.noreply.github.com>
2025-07-09 21:08:36 +0800

    Stored the objects into an array
    adding a new function to handle the adding process of an objct into array
    write a function to loop each object in the array using forEach and display each book on the page

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
+const libraryContainer = document.querySelector("#library-container");
+const addButton = document.querySelector("#add-btn");
+
 const myLibrary = [];
 
-function Book(title, author, pages, read, unread, id) {
+function Book(title, author, pages, status, id) {
   (this.title = title),
     (this.author = author),
     (this.pages = pages),
-    (this.read = read),
-    (this.unread = unread),
-    (this.id = id),
-    (this.info = function () {
-      return (
-        "Book name: " +
-        this.title +
-        " " +
-        "Author: " +
-        this.author +
-        " " +
-        "Pages: " +
-        this.pages
-      );
-    });
+    (this.status = status),
+    (this.id = id);
 }
 
-const theHobbit = new Book("The hobbit", "Rowan", 256);
+function addBookToLibrary(title, author, pages, status) {
+  const id = self.crypto.randomUUID();
 
-console.log(theHobbit);
+  const storedBooks = new Book(title, author, pages, status, id);
 
-function addBookToLibrary(title, author, pages, read, unread, id) {
-  let randomId = self.crypto.randomUUID();
+  myLibrary.push(storedBooks);
 }
+addBookToLibrary("The Hoot", "Nein", 135, "not reading");
+
+addBookToLibrary("The Argo", "Heil", 135, "not reading");
+
+console.log(myLibrary);
+
+function displayBooks() {
+  libraryContainer.innerHTML = "";
+
+  myLibrary.forEach(function (book) {
+    const card = document.createElement("div");
+    card.setAttribute("class", "book-card");
+    card.setAttribute("id", book.id);
+
+    const title = document.createElement("h2");
+    title.textContent = book.title;
+
+    const author = document.createElement("h3");
+    author.textContent = `Author: ${book.author}`;
+
+    const pages = document.createElement("p");
+    pages.textContent = `Pages: ${book.pages}`;
+
+    const status = document.createElement("p");
+    status.setAttribute("class", "card-status");
+    status.textContent = `Status: ${book.status}`;
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(status);
+
+    libraryContainer.appendChild(card);
+  });
+}
+
+displayBooks();
